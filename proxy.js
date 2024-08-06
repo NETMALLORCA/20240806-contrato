a94add278399a0eb881d3f65bd571e2001116ec3
NETMALLORCA <netmallorca@hotmail.com>
2024-08-06 14:42:15 +0200

    update

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@ const app = express();
 
 const options = {
   target: 'http://127.0.0.1:8080', 
-  cookieDomainRewrite: 'dev-aemet.com', 
+  cookieDomainRewrite: 'dev-contrato.com', 
   changeOrigin: true,
   logLevel: 'debug',
   onProxyReq: function(proxyReq, req, res) {
